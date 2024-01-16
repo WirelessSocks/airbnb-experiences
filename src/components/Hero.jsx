a837df65ab108ca4673aa7deceeb272f4a060594
WirelessSocks <romka116rus@icloud.com>
2024-01-17 01:36:02 +0300

    Clean code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Group from "./group.png"
 
 export default function Hero() {
     return (
         <section className='hero'>
-            <img src={Group} alt="group" className='hero--img' />
+            <img src="group.png" alt="group" className='hero--img' />
             <h1 className='hero--header'>Online Experiences</h1>
             <p className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Logo from './airbnb-logo.png';
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={Logo} alt="airbnb logo" className='nav--logo'/>
+            <img src="airbnb-logo.png" alt="airbnb logo" className='nav--logo'/>
         </nav>
     );
 }
